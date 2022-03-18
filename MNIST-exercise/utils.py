d71bf77afd4afa020e0ced2b63aa55ed22ed9b3e
JLGGG <blockboy2957@gmail.com>
2022-03-18 23:44:15 +0900

    Create load_mnist func

---
+++ MNIST-exercise/utils.py
@@ -0,0 +1,20 @@
+import torch
+
+def load_mnist(is_train=True, flatten=True):
+    from torchvision import datasets, transforms
+
+    dataset = datasets.MNIST(
+        '../data', train=is_train, download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+        ]),
+    )
+
+    x = dataset.data.float() / 255. # Normalization
+    y = dataset.targets
+
+    if flatten:
+        x = x.view(x.size(0), -1)
+
+    return x, y
+
