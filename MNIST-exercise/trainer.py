2cd7b6590981f9f0900be5cd79e8e759c024fa76
JLGGG <blockboy2957@gmail.com>
2022-03-22 14:10:03 +0900

    Create trainer.py

---
+++ MNIST-exercise/trainer.py
@@ -0,0 +1,16 @@
+from copy import deepcopy
+from random import random
+import numpy as np
+
+import torch
+
+class Trainer():
+
+    def __init__(self, model, optimizer, crit):
+        self.model = model
+        self.optimizer = optimizer
+        self.crit = crit
+
+        super().__init__()
+
+    
