b7a330edd81c60391e9961728830e5bf4e3b5528
JLGGG <blockboy2957@gmail.com>
2022-03-23 20:02:02 +0900

    Create _batchify func in trainer.py

--- MNIST-exercise/trainer.py
+++ MNIST-exercise/trainer.py
@@ -13,4 +13,15 @@ class Trainer():
 
         super().__init__()
 
+    def _batchify(self, x, y, batch_size, random_split=True):
+        if random_split:
+            indices = torch.randperm(x.size(0), device=x.device)
+            x = torch.index_select(x, dim=0, index=indices)
+            y = torch.index_select(y, dim=0, index=indices)
+
+        x = x.split(batch_size, dim=0)
+        y = y.split(batch_size, dim=0)
+
+        return x, y
+
     
