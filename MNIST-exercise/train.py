99769ed9cc709487e17b4dddea6737f07080c3fc
JLGGG <blockboy2957@gmail.com>
2022-03-27 21:39:08 +0900

    Create main function in train.py

--- MNIST-exercise/train.py
+++ MNIST-exercise/train.py
@@ -32,3 +32,17 @@ def define_argparser():
 
     return config
 
+def main(config):
+    # Set device based on user defined configuration.
+    device = torch.device('cpu') if config.gpu_id < 0 else torch.device('cuda:%d' % config.gpu_id)
+
+    x, y = load_mnist(is_train=True, flatten=True)
+    x, y = split_data(x.to(device), y.to(device), train_ratio=config.train_ratio)
+
+    print("Train:", x[0].shape, y[0].shape)
+    print("Valid:", x[1].shape, y[1].shape)
+
+    input_size = int(x[0].shape[-1])
+    output_size = int(max(y[0])) + 1
+
+    
